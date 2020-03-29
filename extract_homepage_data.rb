50e7371e8b812bc58211118d1ef7cc36eb6e6aee
Egbert <m@egbert.co>
2020-03-29 16:06:24 +0200

    make project filenames sensible

--- extract_homepage_data.rb
+++ extract_homepage_data.rb
@@ -3,33 +3,12 @@ require 'bundler/setup'
 
 Bundler.require :default
 
-index_data = Nokogiri::HTML(Rack::Jekyll.new.call('/').last.first)
-  .css('#projectoverview a')
-  .map.with_index {|p, i|
-    {
-      order: i,
-      orientation: p.css('div').first.attributes['class'].value[/([^ ]*)$/],
-      alinea: p.css('.alinea').text,
-      title: p.css('h1').text,
-      info: p.css('h2').text,
-      page: p.attributes['href'].value
-    }
-  }
-
-
-Dir['project*'].each do |p|
-  match = File.read(p).match Jekyll::Document::YAML_FRONT_MATTER_REGEXP
+Dir['_projects/project*'].each do |p|
+  file_content = File.read(p)
+  match = file_content.match Jekyll::Document::YAML_FRONT_MATTER_REGEXP
   data = SafeYAML.load(match[0])
   content = match.post_match
-  index = index_data.find do |d|
-    p d
-    d[:page] == p
-  end.transform_keys(&:to_s)
-
-  index.delete 'page'
-
-  p 'index', index
-  p 'data', data
   
-  File.write(p, (index.merge(data)).to_yaml + "---\n" + content)
+  filename = data['title'].match(/([^\(]+) \(/)[1].downcase.gsub(/[\. ,\/-]+/, '-')
+  File.write("_projects/#{filename}.html", file_content)
 end
